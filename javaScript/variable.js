b6d8fb436f52508fc03dfe0650afa8f08b43dba8
Shiwangi Dubey <dubeyshiwangi79@gmail.com>
2026-03-27 12:46:20 +0530

    Add files via upload

---
+++ javaScript/variable.js
@@ -0,0 +1,118 @@
+// console.log("hello world");
+// console.log(5+5)
+// part of code which is not execute ----comment
+
+
+// let age;
+// age=24;
+// console.log(age);
+
+// console.log("lovely");
+// let name= "shiwangi";
+// console.log(name);
+
+// let a;
+// let b;
+// let c;
+// a=10;
+// b=20;
+// c=21;
+// console.log(a,b,c);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// let a=10;
+// let b=20;
+// let c=30;
+// console.log(a,b,c);
+
+// let a=30,b=30,c=20;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// let a=10;
+// let a=30;
+
+// var a=10;
+// var a=20;
+
+// var age =1;
+// {
+//     console.log(age);
+
+//     let age1 =2;
+//     console.log(age1);
+// }
+// console.log(age)
+// // console.log(age1);
+// console.log("hello world");
+
+// const pi=3.14;
+// console.log(pi);
+
+// let name= "lovely";
+// let Name="shivi";
+// console.log(name,Name);
+
+// // different type of case exit in programing :-
+// let firstName;// camel case
+// let last_name;//snake case
+// let FullName;//pascal case
+
+// let price1 =390;
+// let price2 =440;
+// let totalPrice=(price1+price2);
+// console.log(totalPrice);
+// console.log(price1+price2);
+// let firstName=prompt("enter name");
+// let lastname=prompt("enter full name");
+// console.log(firstName+lastname);
+
+// let a=1;
+// a="alpha";
+// a=true;
+// console.log(typeof a);
+// console.log(typeof(a));
+
+// console.log("2" +2);
+// console.log(typeof("2" +2));
+// console.log("2" - 2);
+// console.log(typeof("2" -2));
+
+// alpha = 20;
+// console.log(alpha)  not good approach
+
+// let name = null;
+// console.log(typeof(name),name);
+// let name1 ;
+// console.log(name1,typeof name1);
+
+// let number = BigInt(433567878);
+// console.log(number,typeof number);
+
+// let isLoggedIn = false;
+// console.log(isLoggedIn,typeof isLoggedIn);
+
+// let str = "23";
+// let num =Number(str);
+// console.log(num,typeof num);
+
+// let num = prompt("enter the number");
+// console.log(num, typeof num);
+
+// let num = Number(prompt("enter the number"));
+// console.log(num, typeof num);
+// console.log("5" -2);
+
+// console.log("hello world");
+// let number = parseInt("mkl");
+// console.log(number);
+
+
+
+
+
+
+
