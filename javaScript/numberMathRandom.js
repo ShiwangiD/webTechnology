d0952bb6c68658e2c8e81cadf65785072c6ad1cc
Shiwangi Dubey <dubeyshiwangi79@gmail.com>
2026-05-15 08:25:58 +0530

    Add files via upload

---
+++ javaScript/numberMathRandom.js
@@ -0,0 +1,67 @@
+// Number :=
+
+// let a = 10; 
+// console.log(a);
+// let b = 345.6621;
+// console.log(b);
+// console.log(b.toFixed(2));
+// console.log(typeof b.toFixed(3));
+// console.log(typeof b);
+
+// console.log(b.toPrecision(5));// given 5 digit
+// console.log(b.toString());// number convert into string
+
+// let c = new Number(20);
+// console.log(typeof c);
+
+// let d= new Number(20);
+// console.log(c==d);
+
+// let obj1 = {
+//     name:'lovely'
+// };
+// let obj2 = {
+//     name:'lovely'
+// };
+
+// console.log(obj1 == obj2);
+
+// non primitive : Reference base pe compare 
+// primitive : copy by value
+
+// let a = 10;
+// let b = a;
+// console.log(a);
+ // console.log(a == b); // here compare data bases
+
+
+// console.log(Math.abs(-4));
+// console.log(Math.PI);
+// console.log(Math.LN10);
+// console.log(Math.E);
+// console.log(Math.SQRT1_2);
+// console.log(Math.ceil(6.3));
+// console.log(Math.floor(6.3));
+// console.log(Math.log10(20));
+// console.log(Math.max(20,30,49));
+// console.log(Math.min(1,3,4,));
+// console.log(Math.random());
+// console.log(Math.random());
+// console.log(Math.random());// [0-9.99999999]: it will generates value between 0 and 1 ,  where 0 is included but 1 is not included it provide random value 
+
+
+// console.log(Math.floor(Math.random()*10));
+// console.log(Math.floor(Math.random()*10)+1);
+
+// console.log(Math.floor(Math.randow()*6));// math.floor(math.random()*totalNumberOfOutcome) + shift
+// console.log(Math.random()*11);
+
+console.log(Math.floor(Math.random()*11)+15);
+
+// formula :== Math.floor(Math.random(max-min+1))+min
+
+// OTP  generate : 4 digit  : 1000 -9999
+
+
+
+console.log(Math.floor((Math.random()*(9999-1000)+1000)));
